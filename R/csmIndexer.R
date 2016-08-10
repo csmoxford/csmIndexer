6a927489a24b6c3f2bfbe74fa5cb38a154da2d1a
finite2 <dutton.peter@gmail.com>
2016-08-10 11:44:32 +0100

    Get the right filenames for texi2pdf + helpfiles

--- R/csmIndexer.R
+++ R/csmIndexer.R
@@ -2,9 +2,23 @@
 #'
 #' A small indexer which produces a pdf of the contents of a folder.
 #'
-#' @examples
+#' @example inst/example/csmIndexer_example.R
 #'
-#' # Add it here!!!
+#' @param path The path to the folder to be indexed
+#' @param pageLogo The path to logos to add at the top of each page
+#' @param titleLogo The path to logos to add on the title page
+#' @param author The author of the document
+#' @param title The document title
+#' @param personnel The key personnel working on the documents contained in the folder. You may wish to include start and end dates here if appropriate.
+#'
+#' @details
+#' Please set the working directory explicitely using setwd() to ensure you know where the file is being created.
+#'
+#' Note that the R programming language uses bachslash \ to create special characters. The simplest way to write paths is to replace them with forwardslash /.
+#' @author Eleni Frangou
+#' @author Peter Dutton
+#'
+
 #'
 #' @importFrom utils Sweave
 #' @importFrom tools texi2pdf
@@ -13,5 +27,17 @@ csmIndexer = function(){
 
   rnwfile <- system.file("indexer","csmIndexer_v1.0.Rnw", package = "csmIndexer", mustWork = TRUE)
   Sweave(rnwfile)
-  tools::texi2pdf("csmIndexer_v1.0.Rnw", clean = TRUE, quiet = TRUE)
+
+  file = strsplit(rnwfile,"/")[[1]]
+  file = file[length(file)]
+
+  file = substring(file,1,nchar(file)-4)
+  wd = getwd()
+  tools::texi2pdf(paste0(wd,"/",file,".tex"))
+
+  # cleanup:
+  file.remove(paste0(wd,"/",file,".aux"))
+  file.remove(paste0(wd,"/",file,".log"))
+  file.remove(paste0(wd,"/",file,"-concordance.tex"))
+  file.remove(paste0(wd,"/",file,".tex"))
 }
--- inst/example/csmIndexer_example.R
+++ inst/example/csmIndexer_example.R
@@ -1,19 +1,29 @@
-
-
-# install dependencies
-install.packages(c("xtable","stringr"))
 # install package containing the indexer.
 devtools::install_github("csmoxford/csmIndexer")
+
+# Load the library containing the functions
 library(csmIndexer)
 
-setwd("I:/Data/Peter_Dutton/Shared_code/R/indexer")
+#################################################################
+# Note that the R programming language uses bachslash \ to create special characters. The simplest way to write paths is to replace them with forwardslash /.
+#################################################################
 
-path <- "R:/Alzheimers and Dementia/ILiAD"
-pageLogo <- c("I:/Data/OCTRU_SOPs/CSMLogo.png", "I:/Data/OCTRU_SOPs/CSMLogo.png")
-titleLogo <- c("I:/Data/OCTRU_SOPs/CSMLogo.png", "I:/Data/OCTRU_SOPs/CSMLogo.png")
+# Tell R where to create the pdf file
+setwd("I:/Data/Useful Programs/Statistical TMF Index")
+
+# The path to the folder to be indexed
+path <- "I:/Data/Useful Programs/Statistical TMF Index"
+# The path to logos to add at the top of each page
+pageLogo <- c("I:/Data/OCTRU_SOPs/CSMLogo.png")
+# The path to logos to add on the title page
+titleLogo <- c("I:/Data/OCTRU_SOPs/CSMLogo.png")
+# The author of the document
 author <- "Eleni Franou"
-title <- "Trial Master File Statistics Index: ELAD"
-personnel <- c("Eleni Frangou (Statistician)", "Sharon Love (Senior Statistician)")
+# The document title
+title <- "Index of useful Programs Folder"
+# The key personnel working on the documents contained in the folder. You may wish to include start and end dates here if appropriate.
+personnel <- c("Peter Dutton (Statistician)", "Eleni Frangou (Statistician)")
 
+# Create the pdf
 csmIndexer()
 
